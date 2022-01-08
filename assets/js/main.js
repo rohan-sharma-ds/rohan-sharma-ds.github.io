ff959e96b4b5fa096726a0fd8766add6c3f60aa0
Rohan Sharma <Rohanaman22@yahoo.com>
2022-01-08 15:10:42 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,6 +88,102 @@ TxtRotate.prototype.tick = function() {
   }, delta);
 };
 
+function read1() { //finds function
+  var dots = document.getElementById("dots"); //returns element that has the ID attribute with value, searches for dots
+  var moreText = document.getElementById("more"); // '' '' searches for more
+  var btnText = document.getElementById("myBtn"); // '' '' searches for myBtn
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
+function read2() { //finds function
+  var dots = document.getElementById("dots2"); //returns element that has the ID attribute with value
+  var moreText = document.getElementById("more2"); // '' '' searches for more2
+  var btnText = document.getElementById("myBtn2"); // '' '' searches for myBtn2
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
+function read3() { //finds function
+  var dots = document.getElementById("dots3"); //returns element that has the ID attribute with value, searches for dots
+  var moreText = document.getElementById("more3"); // '' '' searches for more
+  var btnText = document.getElementById("myBtn3"); // '' '' searches for myBtn
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
+function read4() { //finds function
+  var dots = document.getElementById("dots4"); //returns element that has the ID attribute with value
+  var moreText = document.getElementById("more4"); // '' '' searches for more2
+  var btnText = document.getElementById("myBtn4"); // '' '' searches for myBtn2
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
+function read5() { //finds function
+  var dots = document.getElementById("dots5"); //returns element that has the ID attribute with value, searches for dots
+  var moreText = document.getElementById("more5"); // '' '' searches for more
+  var btnText = document.getElementById("myBtn5"); // '' '' searches for myBtn
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
+function read6() { //finds function
+  var dots = document.getElementById("dots6"); //returns element that has the ID attribute with value
+  var moreText = document.getElementById("more6"); // '' '' searches for more2
+  var btnText = document.getElementById("myBtn6"); // '' '' searches for myBtn2
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; //button says read more to show more text
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; //button says read less to show less text
+      moreText.style.display = "inline";
+  }
+}
+
 window.onload = function() {
   var elements = document.getElementsByClassName('txt-rotate');
   for (var i=0; i<elements.length; i++) {
